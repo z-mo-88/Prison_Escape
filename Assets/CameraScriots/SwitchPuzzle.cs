8408f4aea75f9ba3642d322448b1a80944395279
DanaK5 <202300513@student.polytechnic.bh>
2026-04-11 11:08:18 +0300

    fixing the zoom

--- Assets/CameraScriots/SwitchPuzzle.cs
+++ Assets/CameraScriots/SwitchPuzzle.cs
@@ -4,68 +4,16 @@ using UnityEngine;
 
 public class SwitchPuzzle : MonoBehaviour
 {
-    public GameObject hintGroup;
-
-    public float zoomDistance = 2.5f;
-
-    private Transform cam;
-    private bool isShowing = false;
-
-    // PUZZLE LOGIC
-    public List<int> correctSequence = new List<int> { 2, 5, 1 };
+    public List<int> correctSequence = new List<int> { 2, 4, 0 };
     private List<int> playerInput = new List<int>();
 
     public Switch[] switches;
-
-    void Start()
-    {
-        cam = Camera.main.transform;
-
-        if (hintGroup != null)
-            hintGroup.SetActive(false);
-    }
-
-    void Update()
-    {
-        if (cam == null) return;
-
-        float distance = Vector3.Distance(cam.position, transform.position);
-
-        if (distance < zoomDistance && !isShowing)
-        {
-            ShowHints();
-        }
-
-        if (distance >= zoomDistance && isShowing)
-        {
-            HideHints();
-        }
-    }
-
-    void ShowHints()
-    {
-        isShowing = true;
-
-        if (hintGroup != null)
-            hintGroup.SetActive(true);
-
-        Debug.Log("HINT SHOW");
-    }
-
-    void HideHints()
-    {
-        isShowing = false;
-
-        if (hintGroup != null)
-            hintGroup.SetActive(false);
-
-        Debug.Log("HINT HIDE");
-    }
 
     public void RegisterInput(int index)
     {
         playerInput.Add(index);
 
+        // check sequence step-by-step
         for (int i = 0; i < playerInput.Count; i++)
         {
             if (playerInput[i] != correctSequence[i])
@@ -75,6 +23,7 @@ public class SwitchPuzzle : MonoBehaviour
             }
         }
 
+        // correct full sequence
         if (playerInput.Count == correctSequence.Count)
         {
             SolvePuzzle();
@@ -83,7 +32,10 @@ public class SwitchPuzzle : MonoBehaviour
 
     void SolvePuzzle()
     {
+        Debug.Log("Puzzle Solved!");
+
         playerInput.Clear();
+
         PuzzleManager.Instance.SolvePuzzle();
     }
 
