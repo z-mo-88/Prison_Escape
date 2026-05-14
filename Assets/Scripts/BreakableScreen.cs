54f90c7b73246f7b271323fac3f976f76a0fd6e7
z-mo-88 <zahraa200466@gmail.com>
2026-05-15 01:25:57 +0300

    add sound

--- Assets/Scripts/BreakableScreen.cs
+++ Assets/Scripts/BreakableScreen.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class BreakableScreen : MonoBehaviour
 {
@@ -7,22 +8,27 @@ public class BreakableScreen : MonoBehaviour
 
     public GameObject blueScreen;
     public GameObject numberObject;
+
     public AudioSource breakingSound;
+    public AudioClip scarySound;
+
     public CameraController cameraController;
 
     private bool isBroken = false;
 
-
     void Start()
     {
-
-        if (cameraController == null &&
-            Camera.main != null)
+        if (cameraController == null && Camera.main != null)
         {
-            cameraController =
-                Camera.main.GetComponent<CameraController>();
+            cameraController = Camera.main.GetComponent<CameraController>();
+        }
+
+        if (numberObject != null)
+        {
+            numberObject.SetActive(false);
         }
     }
+
     void Update()
     {
         if (isBroken)
@@ -33,9 +39,7 @@ public class BreakableScreen : MonoBehaviour
 
         float distance = Vector3.Distance(transform.position, player.position);
 
-
-        if (cameraController != null &&
-            cameraController.IsInteracting())
+        if (cameraController != null && cameraController.IsInteracting())
         {
             if (distance <= breakDistance)
             {
@@ -51,20 +55,45 @@ public class BreakableScreen : MonoBehaviour
                 HammerPickup.nearBreakableScreen = false;
             }
         }
-
-        void BreakScreen()
+        else
         {
-            isBroken = true;
             HammerPickup.nearBreakableScreen = false;
+        }
+    }
 
-            if (breakingSound != null)
-                breakingSound.Play();
+    void BreakScreen()
+    {
+        isBroken = true;
+        HammerPickup.nearBreakableScreen = false;
 
-            if (blueScreen != null)
-                blueScreen.SetActive(false);
+        if (blueScreen != null)
+        {
+            blueScreen.SetActive(false);
+        }
 
-            if (numberObject != null)
-                numberObject.SetActive(true);
+        if (numberObject != null)
+        {
+            numberObject.SetActive(true);
+        }
+
+        StartCoroutine(PlaySounds());
+    }
+
+    IEnumerator PlaySounds()
+    {
+        if (breakingSound != null)
+        {
+            breakingSound.Play();
+
+            if (breakingSound.clip != null)
+            {
+                yield return new WaitForSeconds(breakingSound.clip.length);
+            }
+        }
+
+        if (scarySound != null)
+        {
+            AudioSource.PlayClipAtPoint(scarySound, transform.position);
         }
     }
 }
